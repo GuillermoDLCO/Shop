b3f5741dfbef505753fba198a2c3a0ea2d82e97d
Guillermo De La Cruz <guillermo.dlco@outlook.com>
2019-05-20 00:33:35 -0500

    API Products Controller

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿namespace Shop.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Shop.Web.Data;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public IActionResult GetProducts()
+        {
+            //Va a devolver el resultado envuelto en un json
+            return Ok(this.productRepository.GetAll());
+        }
+    }
+}

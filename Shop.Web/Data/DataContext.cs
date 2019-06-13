fc008071488df047e2d1f5f81e8ff8294d4278a6
Guillermo De La Cruz <guillermo.dlco@outlook.com>
2019-06-13 00:46:13 -0500

    WEB Login / Logout

--- Shop.Web/Data/DataContext.cs
+++ Shop.Web/Data/DataContext.cs
@@ -3,6 +3,7 @@
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
     using Shop.Web.Data.Entities;
+    using System.Linq;
 
     //Se hereda de IdentityDbContext que ya incluye las tablas de usuario
     //que maneja la seguridad integrada del .Net Core y va a trabajar con 
@@ -16,6 +17,30 @@
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //El campo price me lo vas a mapear como tipo decimal en sql
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                //18 numero de los cuales 2 se usaran para el decimal
+                .HasColumnType("decimal(18,2)");
+
+            /*****/
+            //Cuando se vaya a borrar un registro que tenga relacion con otro, no se permita
+            //Va a generar un error
+            var cascadeFKs = modelBuilder.Model
+                .G­etEntityTypes()
+                .SelectMany(t => t.GetForeignKeys())
+                .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Casca­de);
+            foreach (var fk in cascadeFKs)
+            {
+                fk.DeleteBehavior = DeleteBehavior.Restr­ict;
+            }
+            /****/
+
+            base.OnModelCreating(modelBuilder);
         }
 
 
--- Shop.Web/Helpers/UserHelper.cs
+++ Shop.Web/Helpers/UserHelper.cs
@@ -3,16 +3,22 @@
     using System.Threading.Tasks;
     using Data.Entities;
     using Microsoft.AspNetCore.Identity;
-    
+    using Shop.Web.Models;
+
     public class UserHelper : IUserHelper
     {
         private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
 
         //UserHelper va a ser la unica que inyectara el UserManager
         //UserManager No es necesario configurar por el startup, viene del core
-        public UserHelper(UserManager<User> userManager)
+        //signInManager sirve para logear y delogear
+        public UserHelper(
+            UserManager<User> userManager,
+            SignInManager<User> signInManager)
         {
             this.userManager = userManager;
+            this.signInManager = signInManager;
         }
 
         public async Task<IdentityResult> AddUserAsync(User user, string password)
@@ -24,6 +30,23 @@
         {
             return await this.userManager.FindByEmailAsync(email);
         }
+
+        public async Task<SignInResult> LoginAsync(LoginViewModel model)
+        {
+            return await this.signInManager.PasswordSignInAsync(
+                model.Username,
+                model.Password,
+                model.RememberMe,
+                //Bloquee cuenta si esta en true
+                false);
+        }
+
+
+        public async Task LogoutAsync()
+        {
+            await this.signInManager.SignOutAsync();
+        }
+
     }
 
 }
